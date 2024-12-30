188d92166efaf77a59f45baf27248b106727245d
incrediblesaizan1 <glorioussaizan1@gmail.com>
2024-12-30 21:59:32 +0530

    first commit

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,12 @@
+    import React from 'react'
+    
+    const Highlights = () => {
+      return (
+        <div>
+          
+        </div>
+      )
+    }
+    
+    export default Highlights
+    
