ef40aa133627bb5b01eeb1ca840a438570e6006b
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-01-01 14:15:54 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
-
-
+import Features from "./components/Features";
 import * as Sentry from '@sentry/react';
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 
 
 const App = () => {
@@ -16,6 +17,9 @@ const App = () => {
         <Hero />
         <Highlights />
         <Model />
+        <Features />
+        <HowItWorks />
+        <Footer />
       </main>
     </>
   );
